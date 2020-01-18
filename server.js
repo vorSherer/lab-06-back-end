5017d29d052e9690943ef08441dd6d752023a147
Thomas Sherer <olc0710@yahoo.com>
2020-01-18 15:40:08 -0800

    Added location route to server.js.

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 'use strict';
 
 // Environment variables
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+
+// Application setup
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+
+// Routes
+app.get('/', (request, response) => {
+  response.send('Home Page');
+})
+
+app.get('/location', (request, response) => {
+  try{
+    const geoData = require('./data/geo.json');
+    const city = request.query.city;
+    const locationData = new Location(city, geoData);
+    response.send(locationData);
+  }
+  catch(error){
+    errorHandler('So sorry, something went wrong.', request, response);
+  }
+})
+
+// app.get('/weather', (request, response) => {
+//     try{
+//         const geoData = require('./data/geo.json');
+//         const city = request.query.city;
+//         console.log(request.query);
+//         const locationData = new Location(city, geoData);
+//         response.send(locationData);
+//     }
+//     catch(error){
+//         errorHandler('So sorry, something went wrong.', request, response);
+//     }
+// })
 
 
-
-('Proof of Life');
+// Server Listener
+app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
